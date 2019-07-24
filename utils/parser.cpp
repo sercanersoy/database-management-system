ede3504aa4f3cde5971402386141278321ce75d6
sercanersoy <sercan.ersoy@boun.edu.tr>
2019-07-24 17:32:02 +0300

    created parser and constants file and filled constants

---
+++ utils/parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sercan.ersoy on 24.07.2019.
+//
+
+#include "parser.h"
---
+++ utils/parser.h
@@ -0,0 +1,14 @@
+//
+// Created by sercan.ersoy on 24.07.2019.
+//
+
+#ifndef DATABASE_MANAGEMENT_SYSTEM_PARSER_H
+#define DATABASE_MANAGEMENT_SYSTEM_PARSER_H
+
+
+class parser {
+
+};
+
+
+#endif //DATABASE_MANAGEMENT_SYSTEM_PARSER_H
